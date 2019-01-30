a5b0db0998d120c88a1006c39d2cfb0246b24aad
Antonina Maj <antoninajanina@gmail.com>
2019-01-30 15:55:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
 
